565d66e6b0f86f7a226ff49d419d50d65d4a70d8
Will Fox <william.macdonald.fox@gmail.com>
2025-02-25 17:57:09 -0500

    feat: setup merchant serializer, model and first enpoint in controller

---
+++ app/serializers/merchant_serializer.rb
@@ -0,0 +1,26 @@
+class MerchantSerializer
+  def self.format_merchants(merchants)
+    { data:
+      merchants.map do |merchant|
+        {
+          id: merchant.id,
+          type: "merchant",
+          attributes: {
+            name: merchant.name,
+          }
+        }
+      end
+    }
+  end
+
+  def self.format_single(merchant)
+    { data: {
+        id: merchant.id,
+        type: "merchant",
+        attributes: {
+          name: merchant.name,
+        }
+      }
+    }
+  end
+end
