4e10dd052a6cbc3f54208e750c6dda3e4bb23ab0
Will Fox <william.macdonald.fox@gmail.com>
2025-02-25 17:37:47 -0500

    feat: set-up routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # Merchant Routes
+  get "api/v1/merchants", to: "api/v1/merchants#index"
+  get "api/v1/merchants/:id", to: "api/v1/merchants#show"
+  post "api/v1/merchants", to: "api/v1/merchants#create"
+  patch "api/v1/merchants/:id", to: "api/v1/merchants#update"
+  delete "api/v1/merchants/:id", to: "api/v1/merchants#destroy"
+  
+
+
+  # Items Routes
+  get "api/v1/items", to: "api/v1/items#index"
+  get "api/v1/items/:id", to: "api/v1/items#show"
+  post "api/v1/items", to: "api/v1/items#create"
+  patch "api/v1/items/:id", to: "api/v1/items#update"
+  delete "api/v1/items/:id", to: "api/v1/items#destroy"
+
 end
