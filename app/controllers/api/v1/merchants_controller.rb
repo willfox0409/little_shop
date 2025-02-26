746467c84c3e8f567ba36a734d67e0e7ac06a535
Will Fox <william.macdonald.fox@gmail.com>
2025-02-25 19:39:22 -0500

    feat: add foundational CRUD actions, postman responses successful, postman tests not all passing

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -3,4 +3,30 @@ class Api::V1::MerchantsController < ApplicationController
         merchants = Merchant.all
         render json: MerchantSerializer.format_merchants(merchants)
     end
+
+    def show
+        merchant = Merchant.find(params[:id])
+        render json: MerchantSerializer.format_single(merchant)
+    end
+
+    def create
+        merchant = Merchant.create(merchant_params)
+        render json: MerchantSerializer.format_single(merchant)
+    end
+
+    def update
+        merchant = Merchant.update(params[:id], merchant_params)
+        render json: MerchantSerializer.format_single(merchant)
+    end
+
+    def destroy
+        merchant  = Merchant.delete(params[:id])
+        render json: { message: "Merchant deleted successfully" }, status: :no_content
+    end
+
+    private
+
+    def merchant_params
+        params.require(:merchant).permit(:name)
+    end
 end
