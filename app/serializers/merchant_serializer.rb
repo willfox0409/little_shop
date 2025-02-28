91d43db59778d03b23d87ed8aab372673b1dd7ae
Will Fox <william.macdonald.fox@gmail.com>
2025-02-27 21:10:38 -0500

    feat: add count_param argument to MerchantSerializer, to conditionally include item_count in JSON response

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -1,18 +1,21 @@
 class MerchantSerializer
-  def self.format_merchants(merchants)
+  def self.format_merchants(merchants, count_param)
     { data:
       merchants.map do |merchant|
-        {
+        merchant_data = {
           id: merchant.id.to_s,
           type: "merchant",
           attributes: {
             name: merchant.name,
-            item_count: merchant.item_count
           }
         }
-      end,
-     
+        
+        if count_param == "true"
+          merchant_data[:attributes][:item_count] = merchant.item_count 
+        end
 
+        merchant_data
+      end
     }
   end
 
