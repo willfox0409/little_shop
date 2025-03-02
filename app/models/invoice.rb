8485d705080808cd512e1c0ebce8ba9a33fe7665
little-Patrick <Little.patrick@outlook.com>
2025-03-02 13:51:41 -0700

    feat: Add a method to find all of a merchants customers based on merchant id
    TODO: Refactor

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,7 +1,13 @@
 class Invoice < ApplicationRecord
-    belongs_to :merchant
-    belongs_to :customer
-    
-    has_many :invoice_items
-    has_many :transactions
+  belongs_to :merchant
+  belongs_to :customer
+
+  has_many :invoice_items
+  has_many :transactions
+
+  def self.find_merchants_customers(merchant_id)
+    customers_id = find_by_sql(["SELECT customer_id FROM invoices WHERE merchant_id = ?", merchant_id]).pluck(:customer_id)
+    return [] if customers_id.empty?
+    find_by_sql(["SELECT * FROM customers WHERE id IN (?)", customers_id])
+  end
 end
