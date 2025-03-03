fd7d856775c8c5d0311eb36097247ed280872bb4
Will Fox <william.macdonald.fox@gmail.com>
2025-03-02 20:58:32 -0500

    test: add :description properties to test set-up to pass validation

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,14 +7,14 @@ RSpec.describe Item, type: :model do
   end
   it "belongs to a merchant" do
     merchant = Merchant.create!(name: "Store A")
-    item = Item.create!(name: "Product 1", unit_price: 10, merchant: merchant)
+    item = Item.create!(name: "Product 1", description: "banana", unit_price: 10, merchant: merchant)
 
     expect(item.merchant).to eq(merchant)
   end
 
   it "has many invoice items" do
     merchant = Merchant.create!(name: "Store A")
-    item = Item.create!(name: "Product 1", unit_price: 10, merchant: merchant)
+    item = Item.create!(name: "Product 1", description: "banana", unit_price: 10, merchant: merchant)
     invoice = Invoice.create!(customer: Customer.create!(first_name: "John", last_name: "Doe"), merchant: merchant, status: "paid")
     invoice_item = InvoiceItem.create!(invoice: invoice, item: item, quantity: 2, unit_price: 10)
 
