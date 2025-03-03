1a334fe58f4f10cd6171da5c259a24eab930f56b
little-Patrick <Little.patrick@outlook.com>
2025-03-03 14:20:05 -0700

    fix: I don't think a return clause is needed

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -7,7 +7,6 @@ class Invoice < ApplicationRecord
 
   def self.find_merchants_customers(merchant_id)
     customers_id = find_by_sql(["SELECT customer_id FROM invoices WHERE merchant_id = ?", merchant_id]).pluck(:customer_id)
-    return [] if customers_id.empty?
     find_by_sql(["SELECT * FROM customers WHERE id IN (?)", customers_id])
   end
 end
