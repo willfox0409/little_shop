ca59058cbe5cd75a5c596934081a234e16aa0f02
little-Patrick <Little.patrick@outlook.com>
2025-03-03 16:21:18 -0700

    TODO: this added some sad path handlers to the controller, but the sad paths do not pass in postman yet

--- app/controllers/api/v1/items/find_items_controller.rb
+++ app/controllers/api/v1/items/find_items_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::Items::FindItemsController < ApplicationController
   def show 
+    return head :bad_request if params[:name] && (params[:min_price] || params[:max_price])
+
+    return head :bad_request if params[:min_price].to_f < 0 || params[:max_price].to_f < 0
+
     if params[:max_price] || params[:min_price]
       if params[:max_price] && params[:min_price]
         items = Item.full_price(params[:min_price], params[:max_price])
@@ -11,8 +15,10 @@ class Api::V1::Items::FindItemsController < ApplicationController
     elsif params[:name]
       items = Item.find_by_name(params[:name])
     else 
-      #TODO Error handler
+      return head :bad_request
     end
+
     render json: ItemSerializer.format_items(items)
   end
 end
+
