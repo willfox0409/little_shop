8b46346f888e149c666b2a9ded335a5f70dba9a0
little-Patrick <Little.patrick@outlook.com>
2025-03-03 20:53:15 -0700

    fix: add error handling for sad path params

--- app/controllers/api/v1/items/find_items_controller.rb
+++ app/controllers/api/v1/items/find_items_controller.rb
@@ -1,8 +1,10 @@
 class Api::V1::Items::FindItemsController < ApplicationController
-  def show 
-    return head :bad_request if params[:name] && (params[:min_price] || params[:max_price])
-
-    return head :bad_request if params[:min_price].to_f < 0 || params[:max_price].to_f < 0
+  def show
+    if params[:max_price].to_f < 0 || params[:min_price].to_f < 0
+      return render json: ErrorSerializer.new("Price cannot be negative", "400"), status: :bad_request
+    elsif (params[:max_price] && params[:name]) || (params[:min_price] && params[:name])
+      return render json: ErrorSerializer.new("Price and Name can not be queried at the same time", "400"), status: :bad_request
+    end
 
     if params[:max_price] || params[:min_price]
       if params[:max_price] && params[:min_price]
@@ -14,11 +16,12 @@ class Api::V1::Items::FindItemsController < ApplicationController
       end
     elsif params[:name]
       items = Item.find_by_name(params[:name])
-    else 
-      return head :bad_request
+    else
+      return render json: ErrorSerializer.new("Invalid search parameters", status: :bad_request)
     end
 
     render json: ItemSerializer.format_items(items)
   end
+
 end
 
