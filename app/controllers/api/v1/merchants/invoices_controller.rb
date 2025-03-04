682ac68436871122c7ed99ce9d0a42901ccb7770
little-Patrick <Little.patrick@outlook.com>
2025-03-04 15:16:25 -0700

    fix: put in conditional to handle if customer is not found

--- app/controllers/api/v1/merchants/invoices_controller.rb
+++ app/controllers/api/v1/merchants/invoices_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::Merchants::InvoicesController < ApplicationController
   def show 
-    invoices = Invoice.find_merchants_invoices(params[:merchant_id])
-    render json: InvoiceSerializer.format_invoices(invoices)
+    invoices = Invoice.find_merchants_invoices(params[:merchant_id], params[:status])
+
+    if invoices.present?
+      render json: InvoiceSerializer.format_invoices(invoices)
+    else 
+      render json: { error: "Customer not found" }, status: :not_found
+    end
   end
 end
